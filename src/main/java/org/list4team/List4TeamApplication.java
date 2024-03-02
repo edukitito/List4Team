f6c26e59e1cb6e3148a24f260a92a7d99e9cab91
edukitito <edukititodu@gmail.com>
2024-03-01 21:48:49 -0300

    Created Project

---
+++ src/main/java/org/list4team/List4TeamApplication.java
@@ -0,0 +1,13 @@
+package org.list4team;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class List4TeamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(List4TeamApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/list4team/List4TeamApplicationTests.java
@@ -0,0 +1,13 @@
+package org.list4team;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class List4TeamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
