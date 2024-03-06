c914dd3110b834f9ee9018ca35b59597115d1754
edukitito <edukititodu@gmail.com>
2024-03-05 22:03:17 -0300

    More user services

--- src/main/java/org/list4team/services/UserService.java
+++ src/main/java/org/list4team/services/UserService.java
@@ -1,5 +1,6 @@
 package org.list4team.services;
 
+import org.list4team.model.entities.Task;
 import org.list4team.model.entities.User;
 import org.list4team.repositories.UserRepository;
 import org.list4team.services.exceptions.ResourceNotFoundException;
@@ -17,5 +18,32 @@ public class UserService {
     public List<User> findAll(){
         return repository.findAll();
 
+    }
+
+    public User findById(int id){
+        Optional<User> user = repository.findById(id);
+        return user.orElseThrow(() -> new ResourceNotFoundException(id));
+    }
+
+    public User insertUser(User user){
+        return repository.save(user);
+    }
+
+    public void deleteUser(int id){
+        repository.deleteById(id);
+    }
+
+    public User updateUser(int id, User user){
+        User entity = repository.getOne(id);
+        updateData(entity, user);
+        return repository.save(entity);
+    }
+
+    private void updateData(User entity, User user){
+        entity.setNome(user.getNome());
+        entity.setEmail(user.getEmail());
+        entity.setEmail(user.getEmail());
+        entity.setCargo(user.getCargo());
+        entity.setTokem(user.getTokem());
     }
 }
